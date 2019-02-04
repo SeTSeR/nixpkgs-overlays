2e83e567ec25cb4475304a8098943c902be173f0
Sergey Makarov <setser200018@gmail.com>
2019-02-04 20:14:39 +0300

    Update acpilight

--- acpilight/default.nix
+++ acpilight/default.nix
@@ -1,32 +1,28 @@
-{ stdenv, fetchgit, python36 }:
+{ stdenv, fetchgit, python36, udev }:
 stdenv.mkDerivation rec {
   name = "acpilight";
 
   src = fetchgit {
-    url = "https://github.com/wavexx/acpilight";
-    rev = "707a593e23238a00da8015875d2c74da0a249496";
-    sha256 = "1fszya73zgr55dxajd8v0yif96w49r40rrpnfcsi6ac8m903g1wx";
+    url = "https://gitlab.com/wavexx/acpilight.git";
+    rev = "f54865ed9a11eedaeffa71af320a3bf36c89f15d";
+    sha256 = "1b7rx5bf010rxqkxyjcrplxwr32i066lxpqk729x1vhx0y3n0iv4";
   };
 
   pyenv = python36.withPackages (pythonPackages: with pythonPackages; [
     argparse
   ]);
 
-
-  buildInputs = [ pyenv ];
-
-  installPhase = ''
-    mkdir -p $out/etc/udev/rules.d
-    cp 90-backlight.rules $out/etc/udev/rules.d/90-backlight.rules
-    mkdir -p $out/bin
-    cp xbacklight $out/bin/xbacklight
-    gzip xbacklight.1
-    mkdir -p $out/share/man/man1
-    cp xbacklight.1.gz $out/share/man/man1/xbacklight.1.gz
+  postConfigure = ''
+    substituteInPlace Makefile \
+      --replace /usr /
   '';
 
+  buildInputs = [ pyenv udev ];
+
+  makeFlags = [ "DESTDIR=$(out)" ];
+
   meta = {
-    homepage = "https://github.com/wavexx/acpilight";
+    homepage = "https://gitlab.com/wavexx/acpilight";
     description = "ACPI backlight control";
     license = stdenv.lib.licenses.gpl3;
   };
