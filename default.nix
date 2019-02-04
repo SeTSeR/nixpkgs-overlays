e4e0d8801c3a474e86c90811e846deba74632fe0
Sergey Makarov <setser200018@gmail.com>
2019-02-04 21:54:33 +0300

    vim-custom refactoring

--- default.nix
+++ default.nix
@@ -1,18 +1,7 @@
 self: super:
-let
-  vimrc = import vim-custom/vimrc.nix { pkgs = self.pkgs; };
-  vim_configurable = super.pkgs.vim_configurable.override { python = self.python3; };
-in
 {
   acpilight = self.pkgs.callPackage ./acpilight {};
-  vim-custom = vim_configurable.customize {
-      name = "vim-custom";
-      vimrcConfig = {
-          customRC = vimrc.config;
-          packages.myVimPackage = with self.pkgs.vimPlugins; {
-              start = [ auto-pairs rust-vim vim-airline vim-airline-themes vim-wakatime YouCompleteMe ];
-              opt = [ vim-colors-solarized ];
-          };
-      };
+  vim-custom = self.pkgs.callPackage ./vim-custom {
+    vim_configurable = super.pkgs.vim_configurable.override { python = self.python3; };
   };
 }
