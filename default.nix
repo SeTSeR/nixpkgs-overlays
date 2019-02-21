b0cde696e0d11c12db0145dfbcb3de19e76b9175
Sergey Makarov <setser200018@gmail.com>
2019-02-21 09:10:12 +0300

    Compile vim-cpp with python2

--- default.nix
+++ default.nix
@@ -2,9 +2,7 @@ self: super:
 {
   acpilight = self.pkgs.callPackage ./acpilight {};
   vim-generic = self.pkgs.callPackage ./vim-generic {};
-  vim-cpp = self.pkgs.callPackage ./vim-cpp {
-    vim_configurable = super.pkgs.vim_configurable.override { python = self.python3; };
-  };
+  vim-cpp = self.pkgs.callPackage ./vim-cpp {};
   vim-rust = self.pkgs.callPackage ./vim-rust {};
   vk-messenger = self.pkgs.callPackage ./vk-messenger {};
 }
