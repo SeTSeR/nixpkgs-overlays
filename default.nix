b9dc4ed06c33865951ad5261be1b63a5bb479d53
Sergey Makarov <setser200018@gmail.com>
2019-02-21 10:10:02 +0300

    Remove generic vim configuration

--- default.nix
+++ default.nix
@@ -1,7 +1,6 @@
 self: super:
 {
   acpilight = self.pkgs.callPackage ./acpilight {};
-  vim-generic = self.pkgs.callPackage ./vim-generic {};
   vim-cpp = self.pkgs.callPackage ./vim-cpp {};
   vim-rust = self.pkgs.callPackage ./vim-rust {};
   vk-messenger = self.pkgs.callPackage ./vk-messenger {};
