9495a9653f8a7c77db029ecad4fc8eec304ffb34
Sergey Makarov <setser200018@gmail.com>
2019-02-28 19:38:11 +0000

    Remove specific Vim configs from overlays

--- default.nix
+++ default.nix
@@ -1,8 +1,6 @@
 self: super:
 {
   acpilight = self.pkgs.callPackage ./acpilight {};
-  vim-cpp = self.pkgs.callPackage ./vim-cpp {};
-  vim-rust = self.pkgs.callPackage ./vim-rust {};
   vk-messenger = self.pkgs.callPackage ./vk-messenger {};
   cacos = self.pkgs.callPackage ./cacos {};
 }
